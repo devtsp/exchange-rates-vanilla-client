7c473a0aba50b63208ba0ccf3d822c5245a0b7ad
Tomas <87679143+devtsp@users.noreply.github.com>
2022-01-23 19:17:39 -0300

    Main structure
    The API only provides rates with base EUR. The request is the same everytime and the convertion is done by hand.

--- src/main.js
+++ src/main.js
@@ -1,29 +1,56 @@
 const API_URL = 'http://api.exchangeratesapi.io/v1/';
-const latest = 'latest';
 const KEY = '91fd940b557ec6d52701e54ca74445fa';
-const $EUR = document.querySelector('#EUR');
-const $result = document.querySelector('#result');
+const $base = document.querySelector('#base-selection-panel');
+const $baseSelected = document.querySelector('#base-selected');
+const $tbody = document.querySelector('#table-contents');
 
-const fetchData = URL => {
-	fetch(URL);
+$base.onclick = e => {
+	$tbody.classList.toggle('d-none');
+	e.preventDefault();
+	$tbody.replaceChildren();
+	const base = e.target.innerText;
+	if (e.target.nodeName == 'BUTTON') {
+		const URL = `${API_URL}/latest?access_key=${KEY}`;
+		fetchData(URL, base);
+	}
 };
 
-$EUR.onclick = e => {
-	console.log('clicked EUR');
-	fetch(
-		'http://api.exchangeratesapi.io/v1/latest?access_key=91fd940b557ec6d52701e54ca74445fa'
-	)
+const handleSuccess = (data, base) => {
+	for (let currency in data.rates) {
+		const $row = document.createElement('tr');
+		const $currency = document.createElement('th');
+		$currency.setAttribute('scope', 'row');
+		const $rate = document.createElement('td');
+		$currency.innerText = currency;
+		$rate.innerText = data.rates[currency] * (1 / data.rates[base]);
+		if (currency == base) {
+			$rate.innerText = 1;
+		}
+		$tbody.appendChild($row);
+		$row.appendChild($currency);
+		$row.appendChild($rate);
+	}
+	$baseSelected.innerText = `${base}`;
+	$tbody.classList.toggle('d-none');
+};
+
+const handleFail = () => {
+	document.querySelector(
+		'#error-message'
+	).innerText = `An error ocurred fetching the data. Please try again later`;
+};
+
+const fetchData = (URL, base) => {
+	fetch(URL)
 		.then(resp => resp.json())
 		.then(resp => {
-			console.log(resp);
-      handleFetchedData(resp)
+			if (resp.success) {
+				handleSuccess(resp, base);
+			} else {
+				handleFail();
+			}
 		})
 		.catch(err =>
 			console.error('There was an error fetching the requested data: ', err)
 		);
 };
-
-
-const handleFetchedData = data => {
-  $result.innerText = JSON.stringify(data.rates, null, 1)
-} 
