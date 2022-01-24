378d95e4f18dd567c994960f483146454361b138
Tomas <87679143+devtsp@users.noreply.github.com>
2022-01-24 01:42:40 -0300

    Base selection refactor
    Now the base currency is selected via a form/selection input

--- src/main.js
+++ src/main.js
@@ -1,25 +1,59 @@
 const API_URL = 'https://v6.exchangerate-api.com/v6';
 const KEY = 'ec7eec5e7967904472b9cf30';
-const $base = document.querySelector('#base-selection-panel');
-const $baseSelected = document.querySelector('#base-selected');
+const $tableContainer = document.querySelector('#table-container');
 const $tbody = document.querySelector('#table-contents');
 const $errorMessage = document.querySelector('#error-message');
-const $loadingMask = document.querySelector('#loading-mask')
+const $loadingMask = document.querySelector('#loading-mask');
+const $globalExhangeRatesButton = document.querySelector(
+	'#global-exchange-rates'
+);
+const $pairConversionButton = document.querySelector('#pair-conversion');
+const $baseSelectionForm = document.querySelector('#base-selection-form');
+const $baseSelection = document.querySelector('#base-selection');
+const $baseButton = document.querySelector('#base-button');
 
-$base.onclick = e => {
+$globalExhangeRatesButton.onclick = e => {
 	e.preventDefault();
-  $loadingMask.classList.toggle('visually-hidden')
+	$loadingMask.classList.toggle('visually-hidden');
 	$tbody.replaceChildren();
-	const base = e.target.innerText;
-	if (e.target.nodeName == 'BUTTON') {
-		const URL = `${API_URL}/${KEY}/latest/${base}`;
-		fetchData(URL, base);
-	}
+	const URL = `${API_URL}/${KEY}/latest/USD`;
+	fetchData(URL);
+};
+
+$baseButton.onclick = e => {
+	e.preventDefault();
+	$loadingMask.classList.toggle('visually-hidden');
+	$tbody.replaceChildren();
+  base = $baseSelectionForm['base-currency'].value
+	const URL = `${API_URL}/${KEY}/latest/${base}`;
+	fetchData(URL);
+};
+
+const fetchData = URL => {
+	fetch(URL)
+		.then(resp => resp.json())
+		.then(resp => {
+			if (resp.result == 'success') {
+				console.log('success');
+				handleSuccess(resp);
+			} else {
+				console.log('fail');
+				handleFail();
+			}
+		})
+		.catch(err => handleFail(err));
 };
 
 const handleSuccess = data => {
-	for (let currency in data.conversion_rates) {
-		const $row = document.createElement('tr');
+	$baseSelectionForm.classList.remove('visually-hidden');
+	console.log(data);
+	const currencies = data.conversion_rates;
+	for (let currency in currencies) {
+		const $option = document.createElement('option');
+		$option.innerText = currency;
+    $option.value = currency;
+		$baseSelection.appendChild($option);
+    const $row = document.createElement('tr');
 		const $currency = document.createElement('th');
 		$currency.setAttribute('scope', 'row');
 		const $rate = document.createElement('td');
@@ -29,26 +63,14 @@ const handleSuccess = data => {
 		$row.appendChild($currency);
 		$row.appendChild($rate);
 	}
-  $loadingMask.classList.toggle('visually-hidden')
+	$loadingMask.classList.toggle('visually-hidden');
 	$tbody.querySelector('tr:first-child').classList.add('table-success');
+	$tableContainer.classList.remove('visually-hidden');
 };
 
-const handleFail = (err) => {
-  $loadingMask.classList.toggle('visually-hidden')
+const handleFail = err => {
+	$loadingMask.classList.toggle('visually-hidden');
 	$errorMessage.classList.contains('d-none') &&
 		$errorMessage.classList.remove('d-none');
 	$errorMessage.innerText = `An error ocurred with the server: "${err.message}"`;
-};
-
-const fetchData = (URL, base) => {
-  fetch(URL)
-    .then(resp => resp.json())
-    .then(resp => {
-      if (resp.result == 'success') {
-        handleSuccess(resp);
-      } else {
-        handleFail();
-      }
-    })
-    .catch(err => handleFail(err));
 };
