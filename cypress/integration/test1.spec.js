1efc2172df9bd68871cd5d74be2a848bbeb0f59b
Tomas <87679143+devtsp@users.noreply.github.com>
2022-01-30 05:33:17 -0300

    First main test
    Check that all selection elements are filled with  proper option elements to enable the main app interaction

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -2,14 +2,18 @@ const APP_URL = 'http://127.0.0.1:5500';
 const API_URL = 'https://v6.exchangerate-api.com/v6/ec7eec5e7967904472b9cf30/';
 
 describe('Initial DOM setup', () => {
-	it('Fetch the supported currencies and displays them as option objects in both "rates" and "conversion" select form objects', () => {
+	it('Fill all the app <select> elements with fetched data of supported API currencies', () => {
 		cy.intercept('**/codes', req => {
-			delete req.headers['if-none-match'];
 			req.reply({ fixture: 'supported-currencies' });
 		}).as('codes');
 		cy.visit(APP_URL);
 		cy.wait('@codes').then(request => {
-			cy.log(request);
+			const codes = [...request.response.body.supported_codes];
+			cy.get('select').each($select => {
+				$select.children().each((index, $option) => {
+					expect($option.value).to.equal(codes[index][0]);
+				});
+			});
 		});
 	});
 });
